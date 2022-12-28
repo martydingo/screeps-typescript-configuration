1c6980550fc38e7c00c99639c174e5ae235421cf
Martin George <marty@dingopaws.net>
2022-12-28 11:55:04 +0000

    initial commit of production configuration

---
+++ creeps/creepNumbers.ts
@@ -0,0 +1,12 @@
+export const creepNumbers: { [key: string]: number } = {
+  mineSource: 1,
+  feedSpawn: 2,
+  feedTower: 1,
+  feedLink: 1,
+  buildConstructionSite: 2,
+  upgradeController: 1,
+  lootResource: 1,
+  scoutRoom: 1,
+  claimRoom: 1,
+  reserveRoom: 1
+};
