2f492fac5e6b93f4322d73a94580373e44e7bd20
Martin George <marty@dingopaws.net>
2022-12-29 15:35:33 +0000

    Added Terminal & Lab Operation Added Side Room Mining

--- dist/main.js
+++ dist/main.js
@@ -1499,7 +1499,8 @@ class FeedLinkJob {
 const linkConfig = {
     W56N12: {
         "6397f1bd30238608dae79135": "tx",
-        "639864121a5e460386cf8d54": "rx"
+        "639864121a5e460386cf8d54": "rx",
+        "63a3c1f82064b45cf37c59d8": "rx"
     }
 };
 
